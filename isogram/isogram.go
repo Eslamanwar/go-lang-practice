c60648d4b8f6d0ccaee7ea6f9bed755692f88935
Eslam Awad <eslam.awad@dubizzle.com>
2021-03-27 20:48:04 +0400

    test

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,1 +1,21 @@
 package isogram
+
+import (
+	"strings"
+)
+
+// IsIsogram fn
+func IsIsogram(word string) bool {
+	for i, v := range word {
+		if string(v) == "-" || string(v) == " " {
+			continue
+		}
+
+		index := i + 1
+		if strings.Contains(strings.ToUpper(word[index:]), strings.ToUpper(string(v))) {
+
+			return false
+		}
+	}
+	return true
+}
